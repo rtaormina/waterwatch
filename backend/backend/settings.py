7b6e9e6f6be7120b88eff4ba625de8b1a9c476e9
Erik Koprivanacz <E.Koprivanacz@student.tudelft.nl>
2025-05-09 12:53:52 +0200

    Format admin panel for campaigns, add GIS to installed app list

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", default="localhost").split(","
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
+    "django.contrib.gis",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
