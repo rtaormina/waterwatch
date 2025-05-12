dd6195184349056a6b888fb6f4a543a64e9b86b1
TMBood <T.M.Bood@student.tudelft.nl>
2025-05-12 16:06:45 +0200

    change base path in Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/static/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
