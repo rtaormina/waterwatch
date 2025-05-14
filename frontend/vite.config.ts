f7b153f5f901b40c09ce323fdd170c2eb35a6176
Stella Schultz <s.m.schultz@student.tudelft.nl>
2025-05-14 17:46:01 +0200

    add suggested changes to MR

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,11 +8,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  server: {
-    watch: {
-      usePolling: true
-}
-},
   base: '/',
   resolve: {
     alias: {
