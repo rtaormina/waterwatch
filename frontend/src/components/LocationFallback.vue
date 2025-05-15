e70e0a69c506933f3a9ab008ca6b38f8c55f35e2
TMBood <T.M.Bood@student.tudelft.nl>
2025-05-15 12:39:55 +0200

    Add spin.js for loading spinner in LocationFallback component

--- frontend/src/components/LocationFallback.vue
+++ frontend/src/components/LocationFallback.vue
@@ -5,8 +5,8 @@
 <script setup lang="ts">
 import { onMounted, useTemplateRef, watch } from "vue";
 import L from "leaflet";
-
-
+import { Spinner } from "spin.js";
+import "spin.js/spin.css";
 
 const location = defineModel<L.LatLng>("location", {
   default: () => L.latLng(51.999, 4.374),
@@ -33,9 +33,6 @@ marker.on("dragend", (ev: L.DragEndEvent) => {
   location.value = marker.getLatLng();
 });
 
-
-
-
 const mapElement = useTemplateRef("mapElement");
 onMounted(() => {
   if (!mapElement.value) {
@@ -49,23 +46,99 @@ onMounted(() => {
   layer.addTo(map);
   marker.addTo(map);
   map.on("click", setMarkerLocation);
-  map.locate({
-    setView: true,
-    maxZoom: 16,
-  });
-  map.on("locationfound", (ev: L.LocationEvent) => {
-    location.value = ev.latlng;
-  });
-  map.on("locationerror", (e: L.ErrorEvent) => {
-    alert(
-      "Location could not be found set a location manually.\n" + e.message
-    );
-  });
+
+  const locateControl = new LocateControl({ position: "topleft" });
+  locateControl.addTo(map);
 });
 
 function setMarkerLocation(ev: L.LeafletMouseEvent) {
   location.value = ev.latlng;
 }
+
+const LocateControl = L.Control.extend({
+  button: null as HTMLElement | null,
+  icon: null as HTMLElement | null,
+  spinner: null as Spinner | null,
+
+  _createButton: function (): HTMLElement {
+    this.button = L.DomUtil.create("button");
+    this.button.className = "leaflet-bar leaflet-control";
+
+    this.icon = L.DomUtil.create("a");
+    this.icon.textContent = "📍";
+
+    this.button.appendChild(this.icon);
+
+    const spinOpts = {
+      lines: 8, // The number of lines to draw
+      length: 0, // The length of each line
+      width: 8, // The line thickness
+      radius: 10, // The radius of the inner circle
+      scale: 0.5, // Scales overall size of the spinner
+      corners: 1, // Corner roundness (0..1)
+      speed: 0.8, // Rounds per second
+      rotate: 0, // The rotation offset
+      animation: "spinner-line-fade-more", // The CSS animation name for the lines
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: "#000000", // CSS color or array of colors
+      fadeColor: "transparent", // CSS color or array of colors
+    };
+    this.spinner = new Spinner(spinOpts);
+
+    return this.button;
+  },
+
+  _startSpinner: function () {
+    if (!this.icon || !this.spinner) {
+      return;
+    }
+    this.icon.textContent = "";
+    this.spinner.spin(this.icon);
+  },
+  _endSpinner: function () {
+    if (this.spinner) {
+      this.spinner.stop();
+    }
+    if (this.icon) {
+      this.icon.textContent = "📍";
+    }
+  },
+
+  onAdd: function (map: L.Map) {
+    this.button = this._createButton();
+
+    // Add the locate handler
+    L.DomEvent.on(this.button, "click", () => {
+      map.locate({
+        setView: true,
+        maxZoom: 16,
+      });
+      this._startSpinner();
+      map.on("locationfound", (ev: L.LocationEvent) => {
+        this._endSpinner();
+        location.value = ev.latlng;
+      });
+      map.on("locationerror", (e: L.ErrorEvent) => {
+        this._endSpinner();
+        alert(
+          "Location could not be found set a location manually.\n" + e.message
+        );
+      });
+    });
+
+    // Prevent the event from propagating to the map
+    L.DomEvent.disableClickPropagation(this.button);
+
+    return this.button;
+  },
+
+  onRemove: function (map: L.Map) {
+    // Remove the event listener when the control is removed
+    if (this.button) {
+      L.DomEvent.off(this.button);
+    }
+  },
+});
 </script>
 
 <style scoped>
