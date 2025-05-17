9dc4ac59a0d121373ff613b80a7bd4c2f83266e7
TMBood <T.M.Bood@student.tudelft.nl>
2025-05-17 21:56:59 +0200

    add frontend test coverage

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    test: {
-    exclude: ['**/tests-examples/**', '**/tests/e2e/**']
+  test: {
+    include: ['src/**/*.spec.ts', 'tests/unit/**/*.spec.ts'],
+    exclude: ["**/tests-examples/**", "**/tests/e2e/**"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json"],
     },
+  },
+  plugins: [vue()]
 });
