b3b4d7dcac32da1f7ae4d99dab4a63ae11648bb3
TMBood <T.M.Bood@student.tudelft.nl>
2025-05-19 14:11:26 +0200

    Add coverage configuration and update requirements: - Modified .gitlab-ci.yml to integrate coverage reporting for backend and frontend tests. - Updated vitest.config.js to include coverage settings. - Added coverage package to dev-requirements.txt.

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    test: {
-    exclude: ['**/tests-examples/**', '**/tests/e2e/**']
+  test: {
+    include: ["src/**/*.spec.ts", "tests/unit/**/*.spec.ts"],
+    exclude: ["**/tests-examples/**", "**/tests/e2e/**"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "cobertura"],
     },
+  },
 });
