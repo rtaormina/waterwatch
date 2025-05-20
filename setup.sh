3cc458529265a21492663c33cd8ceb35a4672b04
TMBood <T.M.Bood@student.tudelft.nl>
2025-05-20 14:31:12 +0200

    update docker-compose and setup scripts to only do setup on initial creation

--- setup.sh
+++ setup.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 set -e
 
-# Clean start
-docker compose down -v
-docker compose up --build -d
+VOLUME=$(docker volume ls -f name=waterwatch_waterwatch_data --format '{{.Name}}')
+if [ -z "$VOLUME" ];
+then
+    echo "Volume does not exist, creating a new one and importing data."
 
-# Import data
-docker cp countries_1.sql postgres:/countries_1.sql
-docker cp countries_2.sql postgres:/countries_2.sql
-docker compose exec postgres psql -U admin -d pg4django -f countries_1.sql
-docker compose exec postgres psql -U admin -d pg4django -f countries_2.sql
+    # Clean start
+    docker compose down -v
+    docker compose up --build -d
 
-# Run migrations
-docker compose exec backend python manage.py makemigrations
-docker compose exec backend python manage.py migrate
+    # Import data
+    docker cp countries_1.sql postgres:/countries_1.sql
+    docker cp countries_2.sql postgres:/countries_2.sql
+    docker compose exec postgres psql -U admin -d pg4django -f countries_1.sql
+    docker compose exec postgres psql -U admin -d pg4django -f countries_2.sql
 
-# Create superuser and groups
+    # Run migrations
+    docker compose exec backend python manage.py makemigrations
+    docker compose exec backend python manage.py migrate
 
-docker compose exec backend python manage.py groups
+    # Create superuser and groups
 
-# docker compose down
-# docker compose up -d
+    docker compose exec backend python manage.py groups
+
+else
+    echo "Volume already exists, skipping data import."
+
+    # Restart
+    docker compose down
+    docker compose up -d
+
+    docker compose exec backend python manage.py migrate
+fi
