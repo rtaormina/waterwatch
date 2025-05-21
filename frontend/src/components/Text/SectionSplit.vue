b054e89723293f66fa1805b88239f1d794a66c53
TMBood <T.M.Bood@student.tudelft.nl>
2025-05-21 11:46:22 +0200

    Allow Text split to contain up to three sections

--- frontend/src/components/Text/SectionSplit.vue
+++ frontend/src/components/Text/SectionSplit.vue
@@ -3,10 +3,13 @@
     <h2 v-if="title" class="font-bold text-lg">{{ title }}</h2>
     <USeparator v-if="title" />
     <div class="relative flex gap-4">
-      <div class="flex-1">
+      <div v-if="slots.left" class="flex-1">
         <slot name="left" />
       </div>
-      <div class="flex-1">
+      <div v-if="slots.default" class="flex-1">
+        <slot />
+      </div>
+      <div v-if="slots.right" class="flex-1">
         <slot name="right" />
       </div>
     </div>
@@ -14,6 +17,9 @@
 </template>
 
 <script setup lang="ts">
+import { useSlots } from 'vue'
+const slots = useSlots()
+
 defineProps({
   title: {
     type: String,
