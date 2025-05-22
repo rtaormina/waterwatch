4da10a3fa4244433b7da00b8c0f5f28efda8e4fa
Erik Koprivanacz <E.Koprivanacz@student.tudelft.nl>
2025-05-22 13:05:08 +0200

    Add eslint and prettier configurations

---
+++ frontend/eslint.config.js
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
+import prettier from "eslint-config-prettier";
+import pluginPrettier from "eslint-plugin-prettier";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginVue.configs["flat/essential"],
+  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+
+  // Enforce prettier formatting
+  prettier,
+  {
+    plugins: { prettier: pluginPrettier },
+    rules: { "prettier/prettier": "error" },
+  },
+]);
