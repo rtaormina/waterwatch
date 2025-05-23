80a1b136789bfb27dc1cd3259057e7e72b984289
Erik Koprivanacz <E.Koprivanacz@student.tudelft.nl>
2025-05-23 10:45:54 +0200

    Add TypeDoc to linter, update dependencies

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,7 +5,7 @@ import pluginVue from "eslint-plugin-vue";
 import { defineConfig } from "eslint/config";
 import prettier from "eslint-config-prettier";
 import pluginPrettier from "eslint-plugin-prettier";
-
+import jsdoc from "eslint-plugin-jsdoc";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"] },
@@ -20,6 +20,26 @@ export default defineConfig([
   {
     plugins: { prettier: pluginPrettier },
     rules: { "prettier/prettier": "error" },
+  },
+
+  // Enforce TypeDoc style
+  {
+    plugins: { jsdoc },
+    rules: {
+      "jsdoc/require-jsdoc": [
+        "error",
+        {
+          require: {
+            FunctionDeclaration: true,
+            MethodDefinition: true,
+            ClassDeclaration: true,
+            ArrowFunctionExpression: true,
+            FunctionExpression: true,
+          },
+          enableFixer: false,
+        },
+      ],
+    },
   },
 
   // Ignore rules
