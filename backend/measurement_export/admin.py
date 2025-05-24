30b3dfec9463daebb8b7a1a4f48d58c0f09ffd96
Nico Hammer <N.J.Hammer@student.tudelft.nl>
2025-05-24 20:05:27 +0200

    Removed unused libraries

--- backend/measurement_export/admin.py
+++ backend/measurement_export/admin.py
@@ -1,80 +1,14 @@
 """Add Measurement Export to Admin view."""
 
-from django import forms
 from django.contrib import admin
-from django_json_widget.widgets import JSONEditorWidget
 
 from .models import Preset
 
+
 # Register your models here.
-
-
-class PresetAdminForm(forms.ModelForm):
-    """Form for presets in the admin console."""
-
-    class Meta:
-        """Meta class for PresetAdminForm."""
-
-        model = Preset
-        fields = ["name", "description", "filters", "created_by", "is_public"]
-        widgets = {
-            "filters": JSONEditorWidget(
-                options={
-                    "mode": "code",
-                    "mainMenuBar": False,
-                    "statusBar": False,
-                    "schema": {
-                        "type": "object",
-                        "properties": {
-                            "location": {
-                                "type": "object",
-                                "properties": {
-                                    "continents": {"type": "array", "items": {"type": "string"}},
-                                    "countries": {"type": "array", "items": {"type": "string"}},
-                                },
-                            },
-                            "measurements": {
-                                "type": "object",
-                                "properties": {
-                                    "temperature": {
-                                        "type": "object",
-                                        "properties": {
-                                            "from": {"type": "string"},
-                                            "to": {"type": "string"},
-                                            "unit": {"type": "string", "enum": ["C", "F"]},
-                                        },
-                                    }
-                                },
-                            },
-                            "dateRange": {
-                                "type": "object",
-                                "properties": {
-                                    "from": {"type": "string"},
-                                    "to": {"type": "string"},
-                                },
-                            },
-                            "times": {
-                                "type": "array",
-                                "items": {
-                                    "type": "object",
-                                    "properties": {
-                                        "from": {"type": "string"},
-                                        "to": {"type": "string"},
-                                    },
-                                },
-                            },
-                        },
-                    },
-                }
-            ),
-        }
-
-
 @admin.register(Preset)
 class PresetAdmin(admin.ModelAdmin):
     """Admin view for Preset."""
-
-    form = PresetAdminForm
 
     list_display = (
         "name",
