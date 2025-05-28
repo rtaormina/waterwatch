d0570aca18ee5ea674f0b33d68649861cee07c4f
Stella Schultz <s.m.schultz@student.tudelft.nl>
2025-05-28 21:14:42 +0200

    add json locales and internationalization to tutorial

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,33 +1,11 @@
 import { createI18n } from "vue-i18n";
-
-const messages = {
-    en: {
-        Introduction: {
-            title: "Introduction",
-            text:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni vero at ipsam, voluptatibus" +
-                "nemo eum adipisci sunt, deserunt aliquam nihil ipsum quam inventore quibusdam tempore dolorem." +
-                "Enim, et qui. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni vero at ipsam," +
-                " voluptatibus nemo eum adipisci sunt, deserunt aliquam nihil ipsum quam inventore quibusdam" +
-                " tempore dolorem. Enim, et qui.",
-        },
-    },
-    nl: {
-        Introduction: {
-            title: "Introductie",
-            text:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni vero at ipsam, voluptatibus" +
-                "nemo eum adipisci sunt, deserunt aliquam nihil ipsum quam inventore quibusdam tempore dolorem." +
-                "Enim, et qui. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni vero at ipsam," +
-                " voluptatibus nemo eum adipisci sunt, deserunt aliquam nihil ipsum quam inventore quibusdam" +
-                " tempore dolorem. Enim, et qui.",
-        },
-    },
-};
+import en from "./locales/en.json";
 
 export const i18n = createI18n({
     legacy: false,
     locale: "en",
     fallbackLocale: "en",
-    messages,
+    messages: {
+        en,
+    },
 });
--- frontend/src/views/TutorialView.vue
+++ frontend/src/views/TutorialView.vue
@@ -1,100 +1,76 @@
 <template>
     <div>
         <div class="flex items-center justify-between mb-4 px-4 md:px-8">
-            <!-- <div class="w-12"></div> -->
             <div class="flex-1 text-center">
                 <TextTabs :items="tutorials" v-model="page" class="w-full text-center" />
             </div>
             <LanguageSwitcher class="ml-4 border border-main rounded p-2" />
         </div>
-        <!-- <TextTabs :items="tutorials" v-model="page" class="mb-4 w-full text-center" /> -->
         <div class="mx-4 md:mx-8">
+            <!-- How to Take Temperature Measurement -->
             <template v-if="page == 'Measurements'">
-                <TextSection :title="t('Introduction.title')">
-                    <p>{{ t("Introduction.text") }}</p>
+                <TextSection :title="t('measurement.introduction.title')">
+                    <p>{{ t("measurement.introduction.text") }}</p>
                 </TextSection>
                 <TextSectionSplit>
                     <template #left>
                         <USkeleton class="w-full aspect-video"></USkeleton>
                     </template>
                     <template #right>
-                        <TextSubSection title="Step 1">
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni vero at ipsam,
-                                voluptatibus nemo eum adipisci sunt, deserunt aliquam nihil ipsum quam inventore
-                                quibusdam tempore dolorem. Enim, et qui.
-                            </p>
+                        <TextSubSection :title="t('measurement.step1.title')">
+                            <p>{{ t("measurement.step1.text") }}</p>
                         </TextSubSection>
-                        <TextSubSection title="Step 2">
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni vero at ipsam,
-                                voluptatibus nemo eum adipisci sunt, deserunt aliquam nihil ipsum quam inventore
-                                quibusdam tempore dolorem. Enim, et qui.
-                            </p>
+                        <TextSubSection :title="t('measurement.step2.title')">
+                            <p>{{ t("measurement.step2.text") }}</p>
                         </TextSubSection>
-                        <TextSubSection title="Step 3">
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni vero at ipsam,
-                                voluptatibus nemo eum adipisci sunt, deserunt aliquam nihil ipsum quam inventore
-                                quibusdam tempore dolorem. Enim, et qui.
-                            </p>
+                        <TextSubSection :title="t('measurement.step2.title')">
+                            <p>{{ t("measurement.step2.text") }}</p>
                         </TextSubSection>
                     </template>
                 </TextSectionSplit>
-                <TextSection title="Conclusion">
-                    <TextSubSection title="Conclusion">
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni vero at ipsam,
-                            voluptatibus nemo eum adipisci sunt, deserunt aliquam nihil ipsum quam inventore quibusdam
-                            tempore dolorem. Enim, et qui.
-                        </p>
+                <TextSection :title="t('measurement.conclusion.title')">
+                    <TextSubSection :title="t('measurement.conclusion.title')">
+                        <p>{{ t("measurement.conclusion.text") }}</p>
                     </TextSubSection>
-                    <TextSubSection title="Next Steps">
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni vero at ipsam,
-                            voluptatibus nemo eum adipisci sunt, deserunt aliquam nihil ipsum quam inventore quibusdam
-                            tempore dolorem. Enim, et qui.
-                        </p>
+                    <TextSubSection :title="t('measurement.next-steps.title')">
+                        <p>{{ t("measurement.next-steps.text") }}</p>
                     </TextSubSection>
                 </TextSection>
             </template>
+
+            <!-- How to use WATERWATCH Website -->
             <template v-else-if="page == 'Website'">
-                <TextSection title="Introduction">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni vero at ipsam, voluptatibus
-                        nemo eum adipisci sunt, deserunt aliquam nihil ipsum quam inventore quibusdam tempore dolorem.
-                        Enim, et qui. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni vero at ipsam,
-                        voluptatibus nemo eum adipisci sunt, deserunt aliquam nihil ipsum quam inventore quibusdam
-                        tempore dolorem. Enim, et qui.
-                    </p>
+                <TextSection :title="t('website.introduction.title')">
+                    <p>{{ t("website.introduction.text") }}</p>
                 </TextSection>
+
                 <TextSectionSplit>
                     <template #left>
-                        <TextSubSection title="Step 1">
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni vero at ipsam,
-                                voluptatibus nemo eum adipisci sunt, deserunt aliquam nihil ipsum quam inventore
-                                quibusdam tempore dolorem. Enim, et qui.
-                            </p>
+                        <USkeleton class="w-full aspect-video"></USkeleton>
+                    </template>
+                    <template #right>
+                        <TextSubSection :title="t('website.measurement.title')">
+                            <p>{{ t("website.measurement.text") }}</p>
                         </TextSubSection>
-                        <TextSubSection title="Step 2">
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni vero at ipsam,
-                                voluptatibus nemo eum adipisci sunt, deserunt aliquam nihil ipsum quam inventore
-                                quibusdam tempore dolorem. Enim, et qui.
-                            </p>
+                    </template>
+                </TextSectionSplit>
+                <TextSectionSplit>
+                    <template #left>
+                        <TextSubSection :title="t('website.map.title')">
+                            <p>{{ t("website.map.text") }}</p>
                         </TextSubSection>
                     </template>
                     <template #right>
-                        <TextSubSection title="Step 3">
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni vero at ipsam,
-                                voluptatibus nemo eum adipisci sunt, deserunt aliquam nihil ipsum quam inventore
-                                quibusdam tempore dolorem. Enim, et qui. Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Alias facilis beatae quae necessitatibus harum ad, autem rem minus,
-                                mollitia perspiciatis magnam. Aspernatur exercitationem vero ad, enim quae dolorum
-                                doloremque soluta.
-                            </p>
+                        <USkeleton class="w-full aspect-video"></USkeleton>
+                    </template>
+                </TextSectionSplit>
+                <TextSectionSplit>
+                    <template #left>
+                        <USkeleton class="w-full aspect-video"></USkeleton>
+                    </template>
+                    <template #right>
+                        <TextSubSection :title="t('website.export.title')">
+                            <p>{{ t("website.export.text") }}</p>
                         </TextSubSection>
                     </template>
                 </TextSectionSplit>
@@ -115,6 +91,8 @@
                     </TextSubSection>
                 </TextSection>
             </template>
+
+            <!-- How to use WATERWATCH app -->
             <template v-else-if="page == 'MobileApp'">
                 <TextSection title="Introduction">
                     <p>
@@ -173,6 +151,8 @@
                     </TextSubSection>
                 </TextSection>
             </template>
+
+            <!-- How to use WATERWATCH API -->
             <template v-else-if="page == 'API'">
                 <TextSection title="Introduction">
                     <p>
@@ -216,19 +196,19 @@ const page = ref<string>("Measurements");
 
 const tutorials = [
     {
-        label: "How to Take Water Temperature Measurements",
+        label: t("labels.measurement"),
         value: "Measurements",
     },
     {
-        label: "How to Use the WATERWATCH Website",
+        label: t("labels.website"),
         value: "Website",
     },
     {
-        label: "How to Use the WATERWATCH Mobile App",
+        label: t("labels.app"),
         value: "MobileApp",
     },
     {
-        label: "How to Use the WATERWATCH API",
+        label: t("labels.api"),
         value: "API",
     },
 ];
