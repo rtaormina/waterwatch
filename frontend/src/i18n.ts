33b882c52fc6c06eea23bdd91097cd9e4a19e1c4
TMBood <T.M.Bood@student.tudelft.nl>
2025-05-29 19:51:23 +0200

    Add dutch language to language options

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,5 +1,6 @@
 import { createI18n } from "vue-i18n";
 import en from "./locales/en.json";
+import nl from "./locales/nl.json";
 
 export const i18n = createI18n({
     legacy: false,
@@ -7,5 +8,6 @@ export const i18n = createI18n({
     fallbackLocale: "en",
     messages: {
         en,
+        nl,
     },
 });
