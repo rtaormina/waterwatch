8c4a278a702af6c2e9940c6c9ae27d79f1c1ee75
Stella Schultz <s.m.schultz@student.tudelft.nl>
2025-05-30 14:17:20 +0200

    add hex analysis test and fix bugs

--- frontend/src/components/HexAnalysis.vue
+++ frontend/src/components/HexAnalysis.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="bg-white text-center">
-        <h4 class="font-bold mt-2">{{ count }} Measurements</h4>
-        <p>Avg: {{ avgTemp }}°C</p>
-        <p>Min: {{ minTemp }}°C</p>
-        <p>Max: {{ maxTemp }}°C</p>
+        <h4 data-testid="count" class="font-bold mt-2">{{ count }} Measurements</h4>
+        <p data-testid="avg">Avg: {{ avgTemp }}°C</p>
+        <p data-testid="min">Min: {{ minTemp }}°C</p>
+        <p data-testid="max">Max: {{ maxTemp }}°C</p>
 
         <button
+            data-testid="submit"
             class="bg-main text-white px-2 py-1 rounded hover:cursor-pointer"
             @click="
                 props.onOpenDetails();
@@ -28,14 +29,12 @@ const props = defineProps<Props>();
 
 const count = computed(() => props.points.reduce((sum, p) => sum + p.o.count, 0));
 const avgTemp = computed(() => {
-    return (props.points.reduce((sum, p) => sum + p.o.temperature, 0) / props.points.length).toFixed(1);
+    return (props.points.reduce((sum, p) => sum + (p.o.temperature*p.o.count), 0) / props.points.reduce((sum,p) => sum + p.o.count,0)).toFixed(1);
 });
 const minTemp = computed(() => {
-    if (!props.points.length) return "--";
     return Math.min(...props.points.map((p) => p.o.min)).toFixed(1);
 });
 const maxTemp = computed(() => {
-    if (!props.points.length) return "--";
     return Math.max(...props.points.map((p) => p.o.max)).toFixed(1);
 })
 </script>
