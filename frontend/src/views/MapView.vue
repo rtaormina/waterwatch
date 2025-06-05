22b687b871b3f389b614bf347e1d4da52b53a194
Stella Schultz <s.m.schultz@student.tudelft.nl>
2025-06-05 08:15:31 +0200

    add initial mapview test

--- frontend/src/views/MapView.vue
+++ frontend/src/views/MapView.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Modal :visible="firstTime" @close="firstTime = false">
+        <Modal data-testid="modal" :visible="firstTime" @close="firstTime = false">
             <h2 class="text-lg font-semibold mb-4">Welcome to the WATERWATCH Map!</h2>
             <p>
                 View local water quality trends by selecting hexagons or record a measurement by pressing the plus
@@ -17,6 +17,7 @@
             </p>
             <div class="flex items-center mt-4 gap-2">
                 <button
+                    data-testid="view-button"
                     @click="firstTime = false"
                     class="flex-1 bg-main text-white px-4 py-2 rounded mr-2 hover:bg-primary-light hover:cursor-pointer"
                 >
@@ -118,12 +119,12 @@
  */
 defineOptions({ name: "DashboardView" });
 import { PlusCircleIcon } from "@heroicons/vue/24/outline";
-import HexMap from "@/components/HexMap.vue";
+import HexMap from "../components/HexMap.vue";
 import { ref, onMounted, computed } from "vue";
-import MeasurementComponent from "@/components/MeasurementComponent.vue";
-import CampaignBannerComponent from "@/components/CampaignBannerComponent.vue";
+import MeasurementComponent from "../components/MeasurementComponent.vue";
+import CampaignBannerComponent from "../components/CampaignBannerComponent.vue";
 import * as L from "leaflet";
-import DataAnalyticsComponent from "@/components/DataAnalyticsComponent.vue";
+import DataAnalyticsComponent from "../components/DataAnalyticsComponent.vue";
 import { asyncComputed } from "@vueuse/core";
 import Legend from "../components/Legend.vue";
 import { AdjustmentsVerticalIcon } from "@heroicons/vue/24/outline";
@@ -302,10 +303,10 @@ onMounted(async () => {
     /**
      * Display modal only to firsttime users through saving value in localStorage
      */
-    const already = localStorage.getItem("myViewVisited");
+    const already = localStorage.getItem("mapViewVisited");
     if (!already) {
         firstTime.value = true;
-        localStorage.setItem("myViewVisited", "true");
+        localStorage.setItem("mapViewVisited", "true");
     } else {
         firstTime.value = false;
     }
