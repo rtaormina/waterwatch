667fb8cde09267663aee0e61261495a55ccb9348
Erik Koprivanacz <E.Koprivanacz@student.tudelft.nl>
2025-06-09 12:57:17 +0200

    Update legend tests to reflect changes in component

--- frontend/tests/unit/ComponentTests/MapComponents/LegendComponentTest.spec.ts
+++ frontend/tests/unit/ComponentTests/MapComponents/LegendComponentTest.spec.ts
@@ -6,10 +6,11 @@ import Legend from "../../../../src/components/Legend.vue";
 describe('Legend.vue', () => {
   const colors = ['#0000ff', '#ff0000']
   const scale: [number, number] = [10, 50]
+  const colorByTemp: boolean = true
 
   it('renders gradient style correctly', () => {
     const wrapper = mount(Legend, {
-      props: { colors, scale }
+      props: { colors, scale, colorByTemp }
     })
 
     const gradientDiv = wrapper.find('.relative > div')
@@ -19,20 +20,20 @@ describe('Legend.vue', () => {
 
   it('computes step and renders five labels', () => {
     const wrapper = mount(Legend, {
-      props: { colors, scale }
+      props: { colors, scale, colorByTemp }
     })
 
-    const expected = [10, 20, 30, 40, 50].map(n => `${n}°C`)
+    const expected = [`≤10`, `°C`, `20`, `°C`, `30`, `°C`, `40`, `°C`, `≥50`, `°C`]
 
     const labelSpans = wrapper.findAll('.mt-1 span')
-    expect(labelSpans).toHaveLength(5)
+    expect(labelSpans).toHaveLength(10) // 5 labels with 2 spans each
     labelSpans.forEach((span, i) => {
       expect(span.text()).toContain(expected[i])
     })
   })
 
   it('has the correct root class', () => {
-    const wrapper = mount(Legend, { props: { colors, scale } })
+    const wrapper = mount(Legend, { props: { colors, scale, colorByTemp } })
     expect(wrapper.classes()).toContain('legend-popup')
   })
 })
