f78ec41e9b8edf8845c43d246716ac26ef48c0cd
Erik Koprivanacz <E.Koprivanacz@student.tudelft.nl>
2025-06-13 10:51:14 +0200

    Make Legend work again

--- frontend/src/views/ExportMapView.vue
+++ frontend/src/views/ExportMapView.vue
@@ -1,7 +1,5 @@
 <template>
     <div class="w-full h-full flex flex-col p-0 m-0">
-        <CampaignBannerComponent v-if="campaigns.length" :campaigns="campaigns" class="bg-white" />
-
         <div class="w-full h-full flex flex-row">
             <div
                 v-if="viewAnalytics || addMeasurement || showCompareAnalytics"
@@ -45,12 +43,14 @@
                     :selectMult="selectMult && !compareMode"
                     :compareMode="compareMode"
                     :activePhase="comparePhaseNum"
+                    :colorByTemp="colorByTemp"
                     @click="showLegend = false"
                     @hex-click="handleHexClick"
                     @hex-select="handleSelect"
                     @hex-group-select="handleGroupSelect"
                     @open-details="handleOpenAnalysis"
                 />
+
                 <div
                     class="absolute top-4 right-4 flex align-center z-20 justify-center gap-4"
                     v-if="!viewAnalytics && !addMeasurement && !compareMode && !selectMode"
@@ -84,12 +84,15 @@
                 </div>
 
                 <Legend
-                    v-if="showLegend"
-                    class="absolute z-40 mt-1"
+                    v-show="showLegend"
+                    class="absolute z-40 mt-0.95 h-auto"
                     :class="legendClasses"
                     :colors="colors"
                     :scale="scale"
+                    :colorByTemp="colorByTemp"
                     @close="handleCloseAll"
+                    @switch="handleSwitch"
+                    @update="updateMapFilters"
                 />
             </div>
         </div>
@@ -114,7 +117,6 @@ defineOptions({ name: "DashboardView" });
 import HexMap from "../components/HexMap.vue";
 import { ref, computed, nextTick } from "vue";
 import MeasurementComponent from "../components/MeasurementComponent.vue";
-import CampaignBannerComponent from "../components/CampaignBannerComponent.vue";
 import * as L from "leaflet";
 import DataAnalyticsComponent from "../components/Analysis/DataAnalyticsComponent.vue";
 import { asyncComputed } from "@vueuse/core";
@@ -138,7 +140,7 @@ const viewAnalytics = ref(false);
 const addMeasurement = ref(false);
 const showLegend = ref(false);
 const selectMult = ref(false);
-const campaigns = ref([]);
+const colorByTemp = ref(true);
 const hexIntermediary = ref<string>("");
 const hexLocation = ref<string>("");
 
@@ -158,6 +160,8 @@ const count = ref(0);
 const showCompareAnalytics = ref(false);
 const group1Corners = ref<Array<L.LatLng[]>>([]);
 const group2Corners = ref<Array<L.LatLng[]>>([]);
+const range = ref<number[]>([0]);
+const month = ref<string>("0");
 
 // color, styling, and scale values for hexagon visualization
 const colors = ref(["#3183D4", "#E0563A"]);
@@ -171,6 +175,29 @@ const flattenSearchParams = useSearch().flattenSearchParams;
  */
 function returnToExport() {
     router.push({ name: "Export", query: { fromMap: "1" } });
+}
+
+/**
+ * Handles the switch between temperature and count color modes in the legend.
+ */
+function handleSwitch() {
+    colorByTemp.value = !colorByTemp.value;
+    scale.value = colorByTemp.value ? [10, 40] : [0, 50];
+}
+
+/**
+ * Handles filtering observations by time
+ *
+ * @param timeRange the time range of measurements to include in the hexmap
+ * @returns {void}
+ */
+function updateMapFilters(timeRange: number[]) {
+    range.value = timeRange;
+    month.value = "";
+    for (let i = 0; i < range.value.length; i++) {
+        month.value += `${range.value[i]},`;
+    }
+    month.value = month.value.substring(0, month.value.length - 1);
 }
 
 /**
