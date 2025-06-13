47e1a936fc71ba08bf7ef20ee5edbd04cbfd1376
Nico Hammer <N.J.Hammer@student.tudelft.nl>
2025-06-13 13:51:29 +0200

    Add db pooling and redis caching

--- backend/measurement_export/signals.py
+++ backend/measurement_export/signals.py
@@ -9,18 +9,23 @@ from django.db.models.signals import post_delete, post_save
 from measurements.metrics import METRIC_MODELS
 from measurements.models import Measurement
 
+from measurement_export.models import Location
+
 logger = logging.getLogger("WATERWATCH")
 User = get_user_model()
 
-# A list of all models that should trigger a cache clear
-MODELS_TO_INVALIDATE_CACHE = [Measurement, Campaign, User]
+# Models that should trigger clearing the default cache
+MODELS_TO_INVALIDATE_DEFAULT_CACHE = [Measurement, Campaign, User]
 
-# Add metric models to the list of models that invalidate the cache
-MODELS_TO_INVALIDATE_CACHE.extend(METRIC_MODELS)
+# Add metric models to the list of models that invalidate the default cache
+MODELS_TO_INVALIDATE_DEFAULT_CACHE.extend(METRIC_MODELS)
+
+# Models that should trigger clearing the location cache
+MODELS_TO_INVALIDATE_LOCATION_CACHE = [Location]
 
 
-def clear_cache(sender, **_kwargs):
-    """Signal handler to clear the entire cache.
+def clear_default_cache(sender, **_kwargs):
+    """Signal handler to clear the default cache.
 
     This function is connected to the post_save and post_delete signals of specified models.
 
@@ -31,11 +36,36 @@ def clear_cache(sender, **_kwargs):
     **_kwargs : dict
         Additional keyword arguments provided by the signal.
     """
-    logger.info("Cache invalidated due to a change in the %s model.", sender.__name__)
+    logger.info("Default cache invalidated due to a change in the %s model.", sender.__name__)
     cache.clear()
 
 
-# Loop through the list of models and connect the signals
-for model in MODELS_TO_INVALIDATE_CACHE:
-    post_save.connect(clear_cache, sender=model)
-    post_delete.connect(clear_cache, sender=model)
+def clear_location_cache_signal(sender, **_kwargs):
+    """Signal handler to clear the location cache.
+
+    This function is connected to the post_save and post_delete signals of Location model.
+
+    Parameters
+    ----------
+    sender : Model
+        The model class that triggered the signal.
+    **_kwargs : dict
+        Additional keyword arguments provided by the signal.
+    """
+    logger.info("Location cache invalidated due to a change in the %s model.", sender.__name__)
+
+    # Import here to avoid circular imports
+    from .utils import clear_location_cache
+
+    clear_location_cache()
+
+
+# Connect signals for default cache invalidation
+for model in MODELS_TO_INVALIDATE_DEFAULT_CACHE:
+    post_save.connect(clear_default_cache, sender=model)
+    post_delete.connect(clear_default_cache, sender=model)
+
+# Connect signals for location cache invalidation
+for model in MODELS_TO_INVALIDATE_LOCATION_CACHE:
+    post_save.connect(clear_location_cache_signal, sender=model)
+    post_delete.connect(clear_location_cache_signal, sender=model)
