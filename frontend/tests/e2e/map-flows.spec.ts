b9929395aeadd3d52037d7ca8f0122403e81234d
Stella Schultz <s.m.schultz@student.tudelft.nl>
2025-06-22 17:48:51 +0200

    add map flow tests and test-ids

--- frontend/tests/e2e/map-flows.spec.ts
+++ frontend/tests/e2e/map-flows.spec.ts
@@ -3,6 +3,13 @@ import { addMeasurement, clickButton, clickNthHexagonOnMap, moveToCoordinates, z
 
 const url = "http://localhost/";
 
+test.use({
+  // allow geolocation to be used in the tests
+  permissions: ["geolocation"],
+  // stub position to be Amsterdam
+  geolocation: { latitude: 52.0, longitude: 4.0 },
+});
+
 test.describe("Map user flows", () => {
 
     test.beforeEach(async ({ page }) => {
@@ -36,7 +43,7 @@ test.describe("Map user flows", () => {
                 timestamp: "2025-05-26T14:30:00Z",
                 localDate: "2025-05-26",
                 localTime: "14:30:00",
-                latitude: 53.0,
+                latitude: 52.0,
                 longitude: 4.0,
                 waterSource: "well",
                 temperature: {
@@ -64,29 +71,31 @@ test.describe("Map user flows", () => {
         //maybe scroll??
 
         //select sensor
-
-
+        await page.getByTestId("sensor-type").click();
+        await page.locator("text=Analog Thermometer").click();
         //enter temperature
 
+        const tempInput = await page.getByTestId("temp-val");
+        await tempInput.fill("20");
 
         //enter time waited
         const timeWaited = await page.getByTestId("time-waited-mins");
         await timeWaited.fill("5");
 
         //click submit
-        await clickButton(page, "submit-button");
+        await clickButton(page, "submit-measurement");
 
         //click pop up submit button
         await clickButton(page, "submit-button");
 
         //refresh the page to ensure the measurement is saved
-        await page.reload({ waitUntil: "domcontentloaded" });
+        // await page.reload({ waitUntil: "domcontentloaded" });
 
         //zoom in to the map
         await zoomToLevel(page, 10);
 
         //move to the first measurement
-        moveToCoordinates(page, 0, 0);
+        moveToCoordinates(page, 52, 4);
 
         //select the first hexagon
         clickNthHexagonOnMap(page, 0);
@@ -96,6 +105,13 @@ test.describe("Map user flows", () => {
 
         //assert temp in pop up
         await expect(page.locator('text="Min: 10.0°C"')).toBeVisible();
+        await expect(page.locator('text="Max: 30.0°C"')).toBeVisible();
+
+        //assert avg correct in pop up
+        await expect(page.locator('text="Avg: 20.0°C"')).toBeVisible();
+
+        //assert correct number of measurements
+        await expect(page.locator('text="3 Measurements"')).toBeVisible();
 
         //click see details
         await clickButton(page, "submit");
@@ -144,7 +160,7 @@ test.describe("Map legend tests", () => {
                 timestamp: "2025-05-26T14:30:00Z",
                 localDate: "2025-05-26",
                 localTime: "14:30:00",
-                latitude: 53.0,
+                latitude: 52.0,
                 longitude: 4.0,
                 waterSource: "well",
                 temperature: {
@@ -156,26 +172,90 @@ test.describe("Map legend tests", () => {
             url,
         );
 
+        await addMeasurement(
+            page,
+            {
+                timestamp: "2025-05-26T14:30:00Z",
+                localDate: "2025-03-26",
+                localTime: "14:30:00",
+                latitude: 52.0,
+                longitude: 4.0,
+                waterSource: "well",
+                temperature: {
+                    sensor: "Analog Thermometer",
+                    value: 19,
+                    time_waited: 5,
+                },
+            },
+            url,
+        );
+
         await page.goto(url, { waitUntil: "domcontentloaded" });
     });
 
-    test("count selected", async ({ page }) => {
+    test("open legend and toggle date range", async ({ page }) => {
 
-        //move to the first measurement
+        await zoomToLevel(page, 10);
+
         moveToCoordinates(page, 52.0, 4.0);
 
-        //select the first hexagon
+        //open map options
+        await page.getByTestId("open-button").click();
 
+        //open legend
+        await page.getByTestId("map-settings-button").click();
+
+        //select may in the date range selector
+        await page.getByTestId("time-range-select").click();
+        await page.locator("text=May").click();
+
+        //select the first hexagon
+        clickNthHexagonOnMap(page, 0);
+
+        //assert the pop up by finding text
+        await expect(page.locator('text="See Details"')).toBeVisible();
+
+        //assert temp in pop up
+        await expect(page.locator('text="Min: 10.0°C"')).toBeVisible();
+        await expect(page.locator('text="Max: 30.0°C"')).toBeVisible();
+        await expect(page.locator('text="Avg: 20.0°C"')).toBeVisible();
+
+        //assert correct number of measurements
+        await expect(page.locator('text="2 Measurements"')).toBeVisible();
+
+        //open legend again
+        await page.getByTestId("map-settings-button").click();
+
+        //select march in the date range selector
+        await page.getByTestId("time-range-select").click();
+        await page.locator("text=March").click();
+
+        //select the first hexagon
+        clickNthHexagonOnMap(page, 0);
+
+        //assert the correct number of measurements is present
+        await expect(page.locator('text="3 Measurements"')).toBeVisible();
+
+        //open legend
+        await page.getByTestId("map-settings-button").click();
+
+        //select may in the date range selector to only filter to march
+        await page.getByTestId("time-range-select").click();
+        await page.locator('text=/^May$/').click();
+
+        //select the first hexagon
+        clickNthHexagonOnMap(page, 0);
+
+        //assert temp in pop up is only the one measurement in may
+        await expect(page.locator('text="Min: 19.0°C"')).toBeVisible();
+        await expect(page.locator('text="Max: 19.0°C"')).toBeVisible();
+
+        //assert avg correct in pop up
+        await expect(page.locator('text="Avg: 19.0°C"')).toBeVisible();
+
+        //assert correct number of measurements
+        await expect(page.locator('text="1 Measurement"')).toBeVisible();
 
     });
 
-    test("temp selected", async ({ page }) => {
-
-        //move to the first measurement
-        moveToCoordinates(page, 52.0, 4.0);
-
-        //select the first hexagon
-
-
-    });
 });
